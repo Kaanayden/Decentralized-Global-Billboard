8ec3eccc6d4d834f525b55fb64fcf3f17035b962
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-07-24 02:00:37 +0300

    Update RouterPage.js

--- Website/BuyAdd/my-app/src/components/RouterPage.js
+++ Website/BuyAdd/my-app/src/components/RouterPage.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import App from './App';
 import AdUpload from './AdUpload/AdUpload';
+import MetaConnect from './MetaMask/MetaMask';
  
 class RouterPage extends Component {
   render() {
@@ -18,6 +19,9 @@ class RouterPage extends Component {
           </Route>
           <Route path="/upload">
             <AdUpload />
+          </Route>
+          <Route path='/MetaMask'>
+            <MetaConnect/>
           </Route>
         </Switch>
       </div>
