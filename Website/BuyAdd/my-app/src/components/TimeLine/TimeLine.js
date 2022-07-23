8d54673092d42b4a5b3c1d2b6d73f0aaf2d3fb65
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-07-24 00:14:02 +0300

    modal added

--- Website/BuyAdd/my-app/src/components/TimeLine/TimeLine.js
+++ Website/BuyAdd/my-app/src/components/TimeLine/TimeLine.js
@@ -3,7 +3,9 @@ import AddGrid from "../AddGrid/AddGrid";
 import { subMinutes, isBefore, isEqual } from 'date-fns';
 import {Grid, WindowScroller, AutoSizer} from 'react-virtualized';
 import RenderIfVisible from 'react-render-if-visible'
+import AddModal from "../Modal/Modal";
 import './TimeLine.css';
+import { Modal } from "@mui/material";
 
 
 export default function TimeLine(props){
@@ -11,21 +13,28 @@ export default function TimeLine(props){
     const cards = []
     const chosenRef = useRef()
     const [counter, setCounter] = useState(0)
-    const index = 8;
 
     let start = new Date(subMinutes(chosenDate,chosenDate.getHours()*60+chosenDate.getMinutes()))
     //const end = new Date(subMinutes(start,-60*24))
+
     for(var j = 0; j < 1440/4; j++){
         const row = []
         for(var k = 0; k < 4; k++){
           if(isEqual(start,chosenDate)){
-            row.push(<div ref={chosenRef} className="cards-item"><AddGrid chosenDate={start}/></div>)
+            row.push(<div ref={chosenRef} className="cards-item"><AddGrid 
+              chosenDate={start}/>
+              <AddModal addDate={start}/>
+              </div>)
           }
-          else row.push(<div className="cards-item"><AddGrid chosenDate={start}/></div>)
+          else row.push(<div className="cards-item"><AddGrid 
+              chosenDate={start}/>
+              <AddModal addDate={start}/>
+              </div>)
             start = subMinutes(start,-1)
         }
         cards.push(<div className="cards">{row}</div>)
     }
+
     useEffect(()=>{
       window.scrollTo({ behavior: 'smooth', top: chosenRef.current?.offsetTop })
     },[chosenDate])
